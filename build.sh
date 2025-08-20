23c681c494a2a6f93b16c6976ffb89809eab9674
HoikuNavi User <user@example.com>
2025-08-20 13:17:29 +0900

    Gunicornの起動コマンドを修正

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
 
@@ -10,3 +13,6 @@ python manage.py collectstatic --no-input
 
 # Apply migrations
 python manage.py migrate
+
+# Verify gunicorn is installed
+pip list | grep gunicorn
